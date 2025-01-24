118463170ae25ad8d43037acb6c4706c3a4ba2bf
Eyob <eyobsisayy@gmail.com>
2025-01-24 17:06:15 +0300

    list view added to the course

--- src/views/CourseListView.js
+++ src/views/CourseListView.js
@@ -29,9 +29,9 @@ function CourseListView() {
       currency: "ETB",
       email: "abebech_bekele@gmail.com",
       first_name: "Bilen",
-      last_name: "Gizachew",
+     last_name: "Gizachew",
       phone_number: "0912345678",
-      tx_ref: "chewatatest-666909010",
+      tx_ref: "chewatatest-66690901",
       callback_url: "https://webhook.site/077164d6-29cb-40df-ba29-8a00e59a7e60",
       return_url: "https://www.google.com/"
     };
